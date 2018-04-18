a6bc4a8c3394e9ed9b04c1434c1a892af132f62a
kortese <elisakor@metropolia.fi>
2018-04-18 11:59:20 +0300

    Metropolia and kainuu logos

--- hb_footer.php
+++ hb_footer.php
@@ -1,7 +1,11 @@
 <footer class="w3-center">
     <div>
-      <h3>FOOTER</h3>
-      <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+      <br>
+	<h4>Yhteistyössä</h4>
+	<div class="center">
+		<a href="http://www.metropolia.fi/"><img src="http://henkilokohtainenbudjetointi.fi/wp-content/themes/digital-pro/images/footer-logos/Metropolia-nega.png" title="Metropolia logo" alt="Metropolia logo" width="174" height="40"></a>
+		<a href="http://sote.kainuu.fi/index.asp"><img src="http://henkilokohtainenbudjetointi.fi/wp-content/themes/digital-pro/images/footer-logos/kainuu-nega.png" title="Kainuu sote logo" alt="Kainuu sote logo" width="132" height="40"></a>
+	</div>
       <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
     </div>  
       </footer>
