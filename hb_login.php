b1ce4c931dc31c4e3ca818018c21e38355e0e991
katjakkk <38206547+katjakkk@users.noreply.github.com>
2018-05-13 23:03:57 +0300

    valmis

--- hb_login.php
+++ hb_login.php
@@ -1,7 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html lang= "en">
 <head>
     <meta charset="utf-8">
+    
+    <style>
+        body{
+            background-color:  #fea000;
+        }
+    
+    </style>
     <title> HB LOGIN </title>
     
     <link rel="stylesheet" href="css/hb_style.css">
@@ -10,11 +17,11 @@
     
      <h1><br>Henkilökohtainen Budjetointi</h1>
 
-     <img src="http://henkilokohtainenbudjetointi.fi/wp-content/uploads/2017/01/logokuva_oranssi_levea.jpg" alt="HB Logo" width="100%" height="100%">
+     <img id="logo" src="http://henkilokohtainenbudjetointi.fi/wp-content/uploads/2017/01/logokuva_oranssi_levea.jpg" alt="HB Logo">
 
      <?php
      $config = parse_ini_file('../valmisledi/config.ini');
-     $connection = mysqli_connect($config['dbaddr'],$config['usernamecd'],$config['password'],$config['dbname']);
+     $connection = mysqli_connect($config['dbbaddr'],$config['usernamecd'],$config['password'],$config['dbname']);
 
      //jos menee pieleen, virheilmoitus
      if($connection === false){
@@ -23,26 +30,44 @@
      }
 
      mysqli_set_charset($connection,"utf-8");
-     $username = mysqli_real_escape_string($connection,$_POST['username']); /* prevents a bit of SQL injection */
-
-     $qry=mysqli_query($connection,"SELECT * FROM hb_user WHERE usercode='$username'");
-     if(mysqli_num_rows($qry)==1){
-         header("LOCATION:hb_palvelut.php");
-     }
-     else {
-         echo "<div id='error'> Virhe, käyttäjää $username ei löydy.</div>";
-     }
+    session_start(['cookie_lifetime' => 3600]);
+if(empty($_SESSION['username'])){
+    //user is not yet logged in
      ?>
 
-   <form id="mikko2" method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>" align="center">
+   <form id="mikko2" method="post" action="<?php echo htmlentities($_SERVER["PHP_SELF"]); ?>" style="text-align:center; color: white; font-size: 20px;">
        <h2>Kirjaudu sisään</h2>
     Asiakaskoodi: <input id="mikko1" type="password" name="username">
        <br><br>
        <input id="mikko" type="submit" name="clicked" value="Kirjaudu">
     </form>
+    
+    <?php
+     $username = mysqli_real_escape_string($connection,$_POST['username']); /* prevents a bit of SQL injection */
+    
+    $usercode = mysqli_query($connection,"SELECT usercode FROM hb_user");
+    
+     $qry=mysqli_query($connection,"SELECT * FROM hb_user WHERE usercode='$username'");
+    
+    $dbpwd = password_hash($_POST['username'], PASSWORD_DEFAULT);
+     
+      if(!empty($_POST['clicked'])){
+           if(mysqli_num_rows($qry)==1){
+               $_SESSION['usercode']= htmlentities($_POST['username']);   
+         header("LOCATION:hb_palvelut.php");
+     } else{
+               echo "<p id='alert'> Väärä asiakaskoodi</p>";
+           }
+          
+      }
+}else {
+    echo "Hello " . "! <a href='logout.php'> logout</a>";
+}
+    
+    ?>
 
-<h1>
+<div>
     <br><br><br><br><br><br>
-</h1>
+</div>
     </body>
 </html>
